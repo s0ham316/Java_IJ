8f4c5e3f807f2309a7d9cccfddddcf193db72865
s0ham316 <sohamb316@gmail.com>
2021-06-02 08:53:04 +0530

    some new codes from me :D

--- src/Output9.java
+++ src/Output9.java
@@ -2,7 +2,7 @@ public class Output9 {
     public static void main(String[] args){
         int i=7;
         int j=24;
-        int k=++i*8+2-j--;
+        int k = ++i * 8 + 2 - j--;
         System.out.println("k = "+k);
     }
 }

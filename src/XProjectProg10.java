c63537bec8d7f53f7059e2a6469b27ba461e5d43
s0ham316 <sohamb316@gmail.com>
2021-06-15 17:51:11 +0530

    New Codes :D

--- src/XProjectProg10.java
+++ src/XProjectProg10.java
@@ -2,24 +2,33 @@ import java.util.Scanner;
 
 public class XProjectProg10 {
     public static void main(String[] args){
+        //Declaration and Initialization of Variables of main method
         int x=0, n=0;
         Scanner scanner=new Scanner(System.in);
         System.out.println("Enter x");
+        //Taking The Number as Input
         x=scanner.nextInt();
         System.out.println("Enter n");
+        //Taking The Exponent as Input
         n=scanner.nextInt();
+        //Calling the 'display' method to generate series
         display(x,n);
     }
     static void display(int x, int n){
+        //Declaring and Initializing variables as per our needs
         double marker=1,d=0,m=0;
         for (int i=1;i<=n;i++){
+            //Generating and Printing each term and generating their sum
+            //Calling Factorial Method to get the factorial
             m=(Math.pow(x,i)/factorial(i))*marker;
             System.out.println(m);
             d=d+m;
             marker*=-1;
         }
+        //Printing Sum of Series
         System.out.println("Sum of series = "+d);
     }
+    //The 'factorial' Method to get the factorial
     public static int factorial(int n){
         int i,f=1;
         for (i=1;i<=n;i++) {
--- src/XProjectProg6.java
+++ src/XProjectProg6.java
@@ -5,18 +5,18 @@ public class XProjectProg6 {
         System.out.println("Enter a number");
         Scanner scanner=new Scanner(System.in);
         //Taking The Number as Input
-        int num=scanner.nextInt();
+        double num=scanner.nextDouble();
         System.out.println("Enter the exponent");
         //Taking The Exponent as Input
-        int exp=scanner.nextInt();
+        double exp=scanner.nextDouble();
         //Calling the 'power' method to calculate the number raised to the exponent
         double power=power(num, exp);
         //Printing the result
         System.out.println("Power = "+ power);
     }
     //Method 'power' for calculating number raised to the exponent
-    public static double power(int a, int b){
-        int i=1, x=1;
+    public static double power(double a, double b){
+        double i=1, x=1;
         //This if-block executes only for negative exponents
         if (b<0){
             b=b*(-1);
